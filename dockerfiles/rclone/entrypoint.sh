afcfb1d305ff2ab11816b163465cc0879c8b2f14
Jason Boutte <boutte.jason@gmail.com>
2023-12-12 01:11:19 -0800

    Adds CUSTOM_COMMAND to rclone container

--- dockerfiles/rclone/entrypoint.sh
+++ dockerfiles/rclone/entrypoint.sh
@@ -1,11 +1,31 @@
-echo "Creating cron entry using \"${CRON}\" schedule"
-echo "Syncing \"${SOURCE}\" -> \"${DESTINATION}\""
-echo "Using \"${FLAGS}\" for sync command"
+#!/bin/sh
+
+if [[ -n "${CUSTOM_COMMAND}" ]];
+then
+	CRON_ENTRY="${CUSTOM_COMMAND}"
+else
+	CRON_ENTRY="rclone sync ${SOURCE} ${DESTINATION}"
+fi
+
+
+if [[ -n "${PREPARE_SCRIPT}" ]] && [[ -e "${PREPARE_SCRIPT}" ]];
+then
+	CRON_ENTRY="${PREPARE_SCRIPT}; ${CRON_ENTRY}"
+fi
+
+if [[ -n "${FLAGS}" ]];
+then
+	CRON_ENTRY="${CRON_ENTRY} ${FLAGS}"
+fi
+
+echo "Adding cron entry \"${CRON} ${CRON_ENTRY}\""
 
 cat << EOF | crontab -
-${CRON} rclone sync ${SOURCE} ${DESTINATION} ${FLAGS}
+${CRON} ${CRON_ENTRY}
 EOF
 
-CROND_ARGS=${CROND_ARGS:-"-d 8"}
+CROND_ARGS="-f ${CROND_ARGS:--d 8}"
 
-crond -f ${CROND_ARGS}
+echo "Running crond with \"${CROND_ARGS}\""
+
+crond ${CROND_ARGS}
